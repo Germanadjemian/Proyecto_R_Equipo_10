96d9832d69acbb971f5bbd8e383582c9fc79c353
German <germanadjemian@correo.ucu.edu.uy>
2025-07-24 01:22:47 -0300

    Parte 2 pronta

--- Main.R
+++ Main.R
@@ -1,69 +1,86 @@
-#Cargamos las librerías a usar durante el proyecto (algunas quizas no las usamos pero por ahora ante la duda las cargamos, en todo caso luego las sacamos)
+# Cargamos librerías
 library(tidyverse)
 library(readxl)
 library(writexl)
-library(haven) # importar datos de paquetes estadísticos (Stata, SPSS, SAS)
-library(janitor) #Para limpiar
+library(haven)    # importar datos de paquetes estadísticos (Stata, SPSS, SAS)
+library(janitor)  # limpiar nombres
 
+# COMIENZA
+CPI_Japon <- read_csv("2022_Japan_CPI_GoodsAndServiceClassificationIndex.csv") |> 
+  clean_names()  # limpiamos de entrada
 
-#COMIENZA
-#Este es el dataframe original, contiene datos del CPI de Japón desde 1970 hasta 2022
-CPI_Japon <- read_csv("2022_Japan_CPI_GoodsAndServiceClassificationIndex.csv") 
-
-#Primero vamos a ver el resumen, porque tiene 53 filas (observaciones) con 79 datos cada una
-str(CPI_Japon)#Luego de ejecutar esto, lo primero que podemos observar es que todos los datos del datasett son de tipo double
+# Exploración inicial
+str(CPI_Japon)
 summary(CPI_Japon)
 
+# Filtramos solo los ítems individuales (quitamos los agregados)
+all_individual_items <- CPI_Japon |>
+  select(-all_items,
+         -all_items_less_fresh_food,
+         -all_items_less_imputed_rent,
+         -all_items_less_imputed_rent_fresh_food,
+         -all_items_less_fresh_food_and_energy,
+         -all_items_less_food_less_alcoholic_beverages_and_energy)
 
-#Agarro todos los demás elementos por separado y limpio los nombres de las columnas
-all_individual_items <- CPI_Japon|>
-  select(-"All items",-"All items, less fresh food", 
-         -"All items, less imputed rent", -"All items, less imputed rent & fresh food", 
-         -"All items, less fresh food and energy", 
-         -"All items, less food (less alcoholic beverages) and energy") |> 
-  clean_names() 
+# Tomamos los años de princpio y fin de la muestra para ver la variación
+year_1970_individual_items <- all_individual_items |> filter(year == 1970)
+year_2020_individual_items <- all_individual_items |> filter(year == 2020)
 
+# Calculamos variación en 50 años
+variation_50_years <- year_2020_individual_items |> select(-year) - 
+  year_1970_individual_items |> select(-year)
 
-#Saco la columna que tiene NA's
-all_individual_items <- all_individual_items |> 
-                      select(-"water_sewerage_charges")
-
-#Filtro los valores para el año de inicio
-year_1970_individual_items <- all_individual_items |> 
-                              filter(year == 1970)
-
-#Filtro los valores para el año de fin
-year_2020_individual_items <- all_individual_items |> 
-                              filter(year == 2020)
-
-#Calculo la variación de los productos en esos años
-variation_50_years = year_2020_individual_items |> select(-"year") - year_1970_individual_items |> select(-"year")
-
-#Obtengo los valores y 
+# Top 5 variaciones
 values <- as.numeric(variation_50_years[1, ])
 names <- names(variation_50_years)
-
-# Ordenar de mayor a menor y obtener los nombres de las 5 columnas con mayor valor
 top5_names <- names[order(values, decreasing = TRUE)[1:5]]
+variation_top5 <- variation_50_years[, top5_names]
 
-# Extraer esas columnas
-variation_top5 <- variation_50_years[, top5_names, drop = FALSE]
+# CPI general (all items)
+all_items <- CPI_Japon |> select(year, all_items)
 
-#Agarro el CPI de todos los productos juntos según el datasett
-all_items <- CPI_Japon|>
-                      select("year","all_items")
-all_items
-
-
-all_individual_items
-
+# Eliminamos filas con NA's
 all_individual_items_clean <- na.omit(all_individual_items)
-                                         
-#Chequeamos que haya concordancia entre el campo all_items y el promedio de all_individual_items
-all_individual_items_prom <-rowSums(all_individual_items)/length(all_individual_items)
+
+# Comprobación de concordancia con promedio de ítems individuales
+all_individual_items_prom <- rowMeans(all_individual_items |> select(-year), na.rm = TRUE) #Ahora esto hace que descuente los NA y divide entre n columnas menos, equivalente al num de NA que haya en la observación
+all_individual_items_prom_only_with_complete_rows <- rowMeans(all_individual_items_clean |> select(-year))
+
+#Agrego el promedio casero a estos dataframes
+all_individual_items <- all_individual_items |> mutate (prom = rowMeans(select(all_individual_items, -year), na.rm = TRUE)) #El punto es para referirse al datraframe actual, osea all_individual_items
+
+# Resultados
+view(all_items)
+view(all_individual_items)
 all_individual_items_prom
-all_individual_items_prom_cleaned <- rowSums(all_individual_items_clean)/length(all_individual_items_clean)
-all_individual_items_prom_cleaned
+all_individual_items_prom_only_with_complete_rows
 
 
-                                         
+################################### FORMALIDADES ################################### 
+
+###### Parte 2 Análisis del dataset (tomamos el datasett original) #####
+print("PARTE 2 \n")
+#Cantidad de filas y columnas
+print(paste("número de filas:", nrow(CPI_Japon), "|","número de columnas:", ncol(CPI_Japon)))
+
+#Explicar que representa cada fila esta en el informe
+
+#Tipos de las varaibles
+str(CPI_Japon) #Podemos ver que absolutamente todas son col_double()
+
+#Analizar si hay  en términos de todas las variables
+#Para saber esto comparamos el número de filas del datasett original, con el número de filas de un datasett con los duplicados limpiados
+nrow(CPI_Japon) == nrow(distinct(CPI_Japon)) #Devuelve TRUE, por lógica no hay duplicados
+distinct(CPI_Japon)
+
+#Analizar si hay missings NA (spoiler si los hay, al menos hasta la columna 36)
+is.na(CPI_Japon) #Acá nos podemos fijar uno a uno en cada fila (si hay algún true indica un NA)
+sum(is.na(CPI_Japon))#Acá sumamos todos los NA, si da al menos uno es que al menos uno hay, pero esto nos indica la cantidad total de NA en el datasett.
+which(is.na(CPI_Japon), arr.ind = TRUE) #Con esto podemos saber las posiciones exactas de los NA (el arr.ind nos devuelve la posición del NA como si el df fuera una matriz)
+CPI_Japon[1,35] #Ejemplo de como comprobar que las posiciones que nos dio la función anterior son NA efectivamente
+# Podemos observar que aparecen muchos NA en la columna 35 con las primeras observaciones
+#También podemos observar que aparecen muchos NA en las ultimas columnas, en especial en la 79, hasta la 35a observación
+#Todo esto pueden ser indicadores o revelar sesgos o pistas de posibles acontecimientos, por ejemplo que hace tiempo, no se teníam en cuenta estos daatos, quizás porque no se podían medir o no se consideraban importantes.
+
+
+##### Parte 3 Análisis exploratorio de datos (EDA) #####
